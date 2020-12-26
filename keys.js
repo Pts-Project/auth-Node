8d97ddc213690f551dcf980ad9661e3e3840e872
Trisha Sahu <55338588+Trisha-tech@users.noreply.github.com>
2020-12-26 13:29:14 +0530

    make it better

---
+++ keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://trisha:o7nYFVRDPyJY8eMf@cluster0.mc8nq.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET: "tsdrfeuyfjdbvsdjfios",
+    SENDGRID_API: "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",
+    EMAIL: "http://localhost:3000"
+} 
+//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
---
+++ models/product.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const { ObjectId } = mongoose.Schema.Types
+const productSchema = new mongoose.Schema({
+    indexnumber: {
+        type: String,
+        default: "0"
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    countInStock: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+
+    rating: {
+        type: String,
+        default: "0"
+    },
+
+    reviews: [{
+        text: String,
+        postedBy: { type: ObjectId, ref: "User" }
+    }],
+    postedBy: {
+        type: ObjectId,
+        ref: "User"
+    }
+})
+
+mongoose.model("Product", productSchema)
