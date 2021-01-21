7951c09b350b4cc2575fea49ca79fb8abac6a391
Tanmya Vishvakarma <tanmya2000@gmail.com>
2021-01-21 13:32:31 +0530

    trisha new commit

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGOURI: "mongodb+srv://trisha:o7nYFVRDPyJY8eMf@cluster0.mc8nq.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    MONGOURI: "mongodb+srv://Platform:o3EumnIMceQfPiwQ@cluster0.lsibt.mongodb.net/<dbname>?retryWrites=true&w=majority",
     JWT_SECRET: "tsdrfeuyfjdbvsdjfios",
     SENDGRID_API: "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",
     EMAIL: "http://localhost:3000"
